574030b404e6c24ad4deaf588f79bb914e606341
Denis_Budarin <d.budarin@bk.com>
2022-12-08 12:55:17 +0300

    homework save first

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int i = 2;
+int n = 8;
+do
+{
+    if (i%2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
+while (i <= n);
+//end
